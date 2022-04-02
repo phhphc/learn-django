956f93e5b8ccbe312f4d22e799c27c94895010cb
pham phuoc <phhphc@gmail.com>
2022-04-02 14:20:23 +0700

    create urls path

--- learndjango/urls.py
+++ learndjango/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Home page")
-
-def contact(request):
-    return HttpResponse("Contact page")
+from django.urls import path, include
+from pyrsistent import inc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', contact),
+    path('', include('accounts.urls')),
 ]
