bdf0eb48a15ffdcb12c21c80f9480b0cef6be123
pham phuoc <phhphc@gmail.com>
2022-04-04 20:30:21 +0700

    add reset password via email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,42 @@
-from unicodedata import name
+from ossaudiodev import control_labels
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
-    
+
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
-    
+
     path('', views.home, name='home'),
     path('user/', views.userPage, name='user-page'),
-    
+
     path('account/', views.accountSettings, name='account'),
-    
+
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/', views.customer, name='customer'),
 
     path('create_order/<str:pk>', views.create_order, name='create_order'),
     path('update_order/<str:pk>', views.update_order, name='update_order'),
     path('delete_order/<str:pk>', views.delete_order, name='delete_order'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html'),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_form.html'),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_done.html'),
+         name='password_reset_complete'),
+
 ]
